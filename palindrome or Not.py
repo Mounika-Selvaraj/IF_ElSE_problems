f585d6a185748c6f7e8a4b44a94e1f8a0aed8843
Mounika-Selvaraj <146700795+Mounika-Selvaraj@users.noreply.github.com>
2024-07-20 15:24:27 +0530

    Create palindrome or Not.py

---
+++ palindrome or Not.py
@@ -0,0 +1,10 @@
+#Write a Python program to check if a string is a palindrome or not.
+
+char=input("Enter String:")
+reverse=char[::-1]
+print("Char:",char)
+if char==reverse:
+    print(char,"palindrome")
+else:
+    print(char," Is not a Palindrome")
+     
