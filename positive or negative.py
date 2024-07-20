43fb2a8d8d656e482e92801f990fdfbd3c06df40
Mounika-Selvaraj <146700795+Mounika-Selvaraj@users.noreply.github.com>
2024-07-20 14:12:59 +0530

    Create positive or negative.py

---
+++ positive or negative.py
@@ -0,0 +1,8 @@
+#Write a Python program to check if a number is positive or negative.
+num=int(input("enter number:"))
+if (num>0):
+    print("positive")
+elif(num<0):
+    print("Negative")
+else:
+    print("Please Enter Valid Input")
